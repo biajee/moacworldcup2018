ccee1153775829ae2f9d35daba7448b305c3f927
XINLE YANG <biajee@yahoo.com>
2018-06-04 15:50:00 -0700

    Add the small betting contract here so that layman does not need to add data input in the transaction.

---
+++ solidity/ChampionBrazil.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.11;
+//Xinle Yang
+//small contract for people to bet on specific item.
+
+import "./WorldcupFun.sol";
+
+contract ChampionBrazil {
+
+    address public WorldcupFunAddress;
+
+    //constructor
+    function ChampionBrazil(address wf) public {
+        WorldcupFunAddress = wf;
+    }
+
+    function() public payable {
+        //TODO 
+        //Need to make sure this function call is not expensive
+        //0) setup WorldcupFunContract
+        WorldcupFun WorldcupFunContract = WorldcupFun(WorldcupFunAddress);
+        //1) send all money to address in the WorldcupFun
+        if (!WorldcupFunContract.call.value(msg.value)()) revert(); 
+        //2) record the sender address and betting information to WorldcupFun
+        WorldcupFunContract.ChampionBet(msg.sender, "Brazil", msg.value);
+    }
+}
